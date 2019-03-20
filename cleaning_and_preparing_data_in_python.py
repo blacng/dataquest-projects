85fded0d88b3553ce795bdc33280a0a9a87fca27
blacng <blacng@gmail.com>
2019-03-19 20:36:46 -0400

    modified data cleaning py file

--- cleaning_and_preparing_data_in_python.py
+++ cleaning_and_preparing_data_in_python.py
@@ -51,3 +51,101 @@ for row in moma:
     gender = gender.replace(")", "")
     row[2] = nationality
     row[5] = gender
+
+# 5. String Capitalization
+
+for row in moma:
+    '''assigning the gender column to a variable'''
+    gender = row[5]
+    '''assigning the nationality column to a variable'''
+    nationality = row[2]
+    '''converting the gender to title case'''
+    gender = gender.title()
+    '''checking for empty string and updating with a string'''
+    if gender == "":
+        gender = "Gender Unknown/Other"
+    row[5] = gender
+
+    nationality = nationality.title()
+    if nationality == "":
+        nationality = "Nationality Unknown"
+    row[2] = nationality
+
+
+# 6. Errors During Data Cleaning
+
+def clean_and_convert(date):
+    # check that we don't have an empty string
+    if date != "":
+        # move the rest of the function inside
+        # the if statement
+        date = date.replace("(", "")
+        date = date.replace(")", "")
+        date = int(date)
+    return date
+
+for row in moma:
+    BeginDate = row[3]
+    EndDate = row[4]
+
+    BeginDate = clean_and_convert(BeginDate)
+    EndDate = clean_and_convert(EndDate)
+
+    row[3] = BeginDate
+    row[4] = EndDate
+
+# 7. Parsing Numbers from Complex Strings, Part One
+
+bad_chars = ["(",")","c","C",".","s","'", " "]
+
+test_data = ["1912", "1929", "1913-1923",
+             "(1951)", "1994", "1934",
+             "c. 1915", "1995", "c. 1912",
+             "(1988)", "2002", "1957-1959",
+             "c. 1955.", "c. 1970's", 
+             "C. 1990-1999"]
+
+def strip_characters(string):
+    for char in bad_chars:
+        string = string.replace(char, "")
+    return string
+
+stripped_test_data = []
+
+for string in test_data:
+    string = strip_characters(string)
+    stripped_test_data.append(string)
+print(stripped_test_data)
+
+# Output: ['1912', '1929', '1913-1923', '1951', '1994', '1934', '1915', '1995', '1912', '1988', '2002', '1957-1959', '1955', '1970', '1990-1999']
+
+# 8. Parsing Numbers from Complex Strings, Part Two
+
+stripped_test_data = ['1912', '1929', '1913-1923',
+                      '1951', '1994', '1934',
+                      '1915', '1995', '1912',
+                      '1988', '2002', '1957-1959',
+                      '1955', '1970', '1990-1999']
+
+def process_date(date):
+    if "-" in date:
+        split_date = date.split("-")
+        date_one = split_date[0]
+        date_two = split_date[1]       
+        date = (int(date_one) + int(date_two)) / 2
+        date = round(date)
+    else:
+        date = int(date)
+    return date
+
+processed_test_data = []
+
+for d in stripped_test_data:
+    date = process_date(d)
+    processed_test_data.append(date)
+
+for row in moma:
+    date = row[6]
+    date = strip_characters(date)
+    date = process_date(date)
+    row[6] = date
